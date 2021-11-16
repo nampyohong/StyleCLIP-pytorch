32a8ec2a87df29920f00df0b6421e24e6a3a802e
soushirou <nampyo24@kaist.ac.kr>
2021-11-16 05:06:15 +0000

    add pivot tuning inversion

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name='StyleCLIP-pytorch',
+        version=1.0,
+        description='PyTorch implementation of StyleCLIP global direction methods',
+        author='soushirou',
+        author_email='nampyo24@kaist.ac.kr',
+    )
